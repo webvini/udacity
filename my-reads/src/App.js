3fca3d8132710d7d23d77434cf6875dfb62933fa
Vinicius Silva <webvini.silva@gmail.com>
2017-08-23 21:34:14 -0300

    feat: It was created a new  structure

--- my-reads/src/App.js
+++ my-reads/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Content from './content/content.js';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="my-read-wrapper">
+        <div className="header-wrapper">
+          <h1>My Reads</h1>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <Content />
       </div>
     );
   }
