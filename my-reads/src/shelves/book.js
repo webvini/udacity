f594a6b412a349f74039a8fd19cce687ce8485fa
Vinicius Silva <webvini.silva@gmail.com>
2017-08-27 18:47:51 -0300

    feat: added shelf changer

--- my-reads/src/shelves/book.js
+++ my-reads/src/shelves/book.js
@@ -5,7 +5,17 @@ class Book extends Component {
   render() {
     return(
       <div className="book-wrapper">
-        <div className="my-book">{this.props.book.title}</div>
+        <img src={this.props.book.imageLinks.thumbnail} alt={this.props.book.title} />
+        <h3 className="book-title">{this.props.book.title}</h3>
+        
+        <div className="book-shelf-changer">
+          <select>
+            <option value="none" disabled>Move to...</option>
+            <option value="currentlyReading">Currently Reading</option>
+            <option value="wantToRead">Want to Read</option>
+            <option value="read">Read</option>
+          </select>
+        </div>
       </div>
     )
   }
