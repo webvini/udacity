af5da9d4685c686f8105b0c887e88b9ad6c34f8d
Vinicius Silva <webvini.silva@gmail.com>
2017-09-03 15:12:08 -0300

    feat: added search component and some fix

--- my-reads/src/search/search.js
+++ my-reads/src/search/search.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
+import escapeRegExp from 'escape-string-regexp'
 import { Link } from 'react-router-dom';
+import * as BooksAPI from './../BooksAPI.js';
+
+import Book from './../shelves/book.js';
 import './search.css';
 
 class Search extends Component {
+
+  state = {
+    query: '',
+    books: []
+  }
+
+  onSearch = (el) => {
+    let value = el.target.value.trim();
+    (value) ? this.getBooks(value) : false;
+  }
+
+  getBooks = (query) => {
+    BooksAPI.search(query, 20).then((books) => {
+      if(!books.error) {
+        this.setState({ books });
+      }
+    })
+  }
+
   render() {
     return(
       <div className="search-wrapper">
@@ -10,21 +33,18 @@ class Search extends Component {
           <Link to='/' className="close-search">Close</Link>
 
           <div className="search-books-input-wrapper">
-            {/* 
-              NOTES: The search from BooksAPI is limited to a particular set of search terms.
-              You can find these search terms here:
-              https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-              
-              However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-              you don't find a specific author or title. Every search is limited by search terms.
-            */}
-            <input type="text" placeholder="Search by title or author"/>
-            
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={this.onSearch}
+            />
           </div>
         </div>
-
-        <div className="search-books-results">
-          <ol className="books-grid"></ol>
+        
+        <div className="listing">
+          {this.state.books.map((book) => (
+            <Book key={book.id} book={book} />
+          ))}
         </div>
       </div>
     )
