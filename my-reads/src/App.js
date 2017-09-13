17b3286977dc9eca3fb73f9b34106113d0c95a57
Vinicius Silva <webvini.silva@gmail.com>
2017-09-12 21:17:59 -0300

    feat: created feature for search

--- my-reads/src/App.js
+++ my-reads/src/App.js
@@ -30,10 +30,6 @@ class App extends Component {
     return <Shelves key={shelf.flag} shelf={shelf.title} books={this.state.books.filter(book => book.shelf === shelf.flag)} updatedBook={this.updatedBook} />
   }
 
-  renderSearch = book => {
-    return <Search key={book.id} myBook={book} />
-  }
-
   render() {
     let shelves = [
       {flag: "currentlyReading", title: "Currently Reading"},
@@ -60,23 +56,7 @@ class App extends Component {
 
           <Route path='/search' render={() => (
             <div className="content-wrapper">
-              <div className="search-wrapper">
-                <div className="search-books-bar">
-                  <Link to='/' className="close-search">Close</Link>
-
-                  <div className="search-books-input-wrapper">
-                    <input
-                      type="text"
-                      placeholder="Search by title or author"
-                      onChange={this.onSearch}
-                    />
-                  </div>
-                </div>
-              </div>
-              
-              <div className="listing">
-                {this.state.books.map(this.renderSearch)}
-              </div>
+              <Search books={this.state.books} />
             </div>
           )}/>
         </div>
--- my-reads/src/search/search.js
+++ my-reads/src/search/search.js
@@ -13,32 +13,46 @@ class Search extends Component {
     allBooks: []
   }
 
-  componentDidMount() {
-    // this.props.myBooks.map((book) => (
-    //   this.setState({
-    //     allBook: book.filter((book) => book.id !== this.state.allBook.id)
-    //   })
-    // ))
-  }
-
   onSearch = (el) => {
     let value = el.target.value.trim();
     (value) ? this.getAllBooks(value) : false;
   }
 
-  // getAllBooks = (query) => {
-  //   BooksAPI.search(query, 20).then((books) => {
-  //     if(!books.error) {
-  //       this.setState({
-  //         allBooks: books.filter((myBook) => myBook.id !== books.id)
-  //       });
-  //     }
-  //   })
-  // }
+  getAllBooks = (query) => {
+    BooksAPI.search(query, 20).then((books) => {
+      if(!books.error) {
+        let allBooks = books.map(book => {
+          let foundMyBook = this.props.books.filter(myBook => book.id === myBook.id)[0]
+          if(foundMyBook){
+            book.shelf = foundMyBook.shelf
+          }
+          return book
+        })
+
+        this.setState({ allBooks })
+      }
+    })
+  }
 
   render() {
     return(
-      <Book key={this.props.myBook.id} book={this.props.myBook} />
+      <div className="search-wrapper">
+        <div className="search-books-bar">
+          <Link to='/' className="close-search">Close</Link>
+
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              onChange={this.onSearch}
+            />
+          </div>
+        </div>
+
+        {this.state.allBooks.map((book) => (
+          <Book key={book.id} book={book} />
+        ))}
+      </div>
     )
   }
 }
