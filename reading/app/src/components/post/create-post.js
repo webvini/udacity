d976b320227c60217dcb9e04d3688cc00a869e39
Vinicius Silva <webvini.silva@gmail.com>
2017-11-15 01:56:08 -0200

    feature: start create post component

---
+++ reading/app/src/components/post/create-post.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class CreatePost extends Component {
+  render() {
+    return (
+      <div className="create-post-wrapper">
+        <h2>Create new post</h2>
+      </div>
+    )
+  }
+}
+
+export default CreatePost
