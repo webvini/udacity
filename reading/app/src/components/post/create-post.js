131c594040d0ac6edbd2a6161b01bc5798307d8d
Vinicius Silva <webvini.silva@gmail.com>
2017-11-15 18:12:56 -0200

    feature: form create and edit post component

--- reading/app/src/components/post/create-post.js
+++ reading/app/src/components/post/create-post.js
@@ -15,10 +15,27 @@ class CreatePost extends Component {
       <div className="create-post-wrapper">
         <h2>Create new post</h2>
 
-        <form onSubmit={this.handleSubmit}>
+        <form className="form-wrapper" onSubmit={this.handleSubmit}>
           <label>
             <span>Title</span>
             <input type="text" name="title" />
+          </label>
+
+          <label>
+            <span>Author</span>
+            <input type="text" name="author" />
+          </label>
+
+          <label>
+            <span>Category</span>
+            <select>
+              <option>React</option>
+            </select>
+          </label>
+
+          <label>
+            <span>Body</span>
+            <textarea name="body"></textarea>
           </label>
 
           <button type="submit" className="btn">Send</button>
