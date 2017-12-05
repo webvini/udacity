28957960e2b9172d95fb8d623509387b3bed553d
Vinicius Silva <webvini.silva@gmail.com>
2017-12-04 22:38:44 -0200

    feature: form style

--- flashcards/components/newDeck.js
+++ flashcards/components/newDeck.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, TextInput, Button } from 'react-native'
 
 class NewDeck extends Component {
+  onCreate = () => {
+    console.log('ahhaha')
+  }
+
   render() {
     return (
       <View style={styles.newDeckWrapper}>
         <Text style={styles.title}>What is the title of your new deck?</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Deck title"
+        />
+
+        <Button
+          onPress={this.onCreate}
+          title="Submit"
+        />
       </View>
     )
   }
@@ -19,6 +33,13 @@ const styles = StyleSheet.create({
   title: {
     fontSize: 24,
     textAlign: 'center'
+  },
+  input: {
+    marginTop: 20,
+    marginBottom: 20,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ccc'
   }
 })
 
