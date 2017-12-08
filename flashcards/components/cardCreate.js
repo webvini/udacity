df60c569ab0fad92a78e4a33fb62931b3eaa3d2f
Vinicius Silva <webvini.silva@gmail.com>
2017-12-08 13:24:58 -0200

    feature: remove validation for button

--- flashcards/components/cardCreate.js
+++ flashcards/components/cardCreate.js
@@ -2,39 +2,22 @@ import React, { Component } from 'react'
 import { StyleSheet, View, Text, TextInput, Button } from 'react-native'
 
 class CardCreate extends Component {
-
-  state = {
-    disabledButton: true
-  }
-
-  onChangedText = title => {
-    this.setState({
-      disabledButton: !title
-    })
-  }
-
   render() {
-
-    console.log(this.props)
-
     return (
       <View style={styles.cardWrapper}>
         <TextInput
           style={styles.input}
           placeholder="Card title"
-          onChangeText={this.onChangedText}
         />
 
         <TextInput
           style={styles.input}
           placeholder="Card description"
-          onChangeText={this.onChangedText}
         />
 
         <Button
           onPress={() => navigation.navigate('cardCreate')}
           title="Submit"
-          disabled={this.state.disabledButton}
         />
       </View>
     )
