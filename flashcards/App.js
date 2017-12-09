4e72c839437577dce7626923111d35e5edb2802d
Vinicius Silva <webvini.silva@gmail.com>
2017-12-09 13:01:13 -0200

    feature: start redux config

--- flashcards/App.js
+++ flashcards/App.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
+import thunkMiddleware from 'redux-thunk'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import configureStore from './store'
 
+import reducer from './reducers'
 import Home from './components/home/index'
 
-const store = configureStore()
+const store = createStore(
+  reducer,
+  applyMiddleware(
+    thunkMiddleware
+  )
+)
 
 class App extends Component {
   render() {
