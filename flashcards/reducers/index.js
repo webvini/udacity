cd6dbad016c1f8f3b1fa0a26bbd3e2fc7f5a87e1
Vinicius Silva <webvini.silva@gmail.com>
2017-12-09 13:14:30 -0200

    feature: added initial state

--- flashcards/reducers/index.js
+++ flashcards/reducers/index.js
@@ -1,7 +1,32 @@
 import { AsyncStorage } from 'react-native'
 import { ADD_DECKS, RECEIVE_DECKS } from '../actions'
 
-const decks = (state = {}, action) => {
+const init = {
+  React: {
+    title: 'React',
+    questions: [
+      {
+        question: 'What is React?',
+        answer: 'A library for managing user interfaces'
+      },
+      {
+        question: 'Where do you make Ajax requests in React?',
+        answer: 'The componentDidMount lifecycle event'
+      }
+    ]
+  },
+  JavaScript: {
+    title: 'JavaScript',
+    questions: [
+      {
+        question: 'What is a closure?',
+        answer: 'The combination of a function and the lexical environment within which that function was declared.'
+      }
+    ]
+  }
+}
+
+const decks = (state = init, action) => {
   const { deck } = action
 
   switch(action.type) {
