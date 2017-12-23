643102e10f403e3f0e488da0b70e9c55cd5d0a8a
Vinicius Silva <webvini.silva@gmail.com>
2017-12-23 00:45:50 -0200

    fix

--- flashcards/components/card/index.js
+++ flashcards/components/card/index.js
@@ -7,6 +7,8 @@ class Card extends Component {
 
   state = {
     title: '',
+    cardTitle: '',
+    description: '',
     allDecks: []
   }
 
@@ -22,41 +24,60 @@ class Card extends Component {
 
   isNewDeck = () => {
     const { allDecks, title } = this.state
+    const result = Object.values(allDecks).filter((deck) => deck.title === title)
 
-    Object.values(allDecks).map((deck) => {
-      if(deck.title === title) {
-        console.log('Opa! já temos esse deck')
-      }else{
-        console.log('Deck novo criado com sucesso')
-      }
-    })
+    return (result.length <= 0) ? true : false
   }
 
   getAllDecks = () => {
     AsyncStorage.getItem('decks').then((decks) => {
       this.setState({
         allDecks: JSON.parse(decks)
-      }, () => {
-        this.isNewDeck()
       })
     })
+  }
+
+  handleInputCardTitle = el => {
+    this.setState({
+      cardTitle: el
+    })
+  }
+
+  handleInputDescription = el => {
+    this.setState({
+      description: el
+    })
+  }
+
+  onCreate = () => {
+    if(this.isNewDeck()) {
+      console.log('é novoooo')
+    }else{
+      console.log('ja existe')
+    }
   }
 
   render() {
     return (
       <View style={styles.cardWrapper}>
         <TextInput
+          name="cardTitle"
           style={styles.input}
           placeholder="Card title"
+          onChangeText={this.handleInputCardTitle}
+          value={this.state.cardTitle}
         />
 
         <TextInput
+          name="description"
           style={styles.input}
           placeholder="Card description"
+          onChangeText={this.handleInputDescription}
+          value={this.state.description}
         />
 
         <Button
-          onPress={() => navigation.navigate('card')}
+          onPress={() => this.onCreate()}
           title="Submit"
         />
       </View>
